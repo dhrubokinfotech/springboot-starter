60005bccf54306311cc03ef0aae1f0e87ea7a575
zakaria5729 <zakariahossain143@gmail.com>
2023-02-23 13:02:31 +0600

    added GlobalExceptionHandler

---
+++ src/main/java/com/disl/boilerplate/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.disl.boilerplate.exceptions;
+
+import com.disl.boilerplate.models.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        Response errorResponse = new Response(HttpStatus.NOT_FOUND, false, ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
+        Response errorResponse = new Response(HttpStatus.INTERNAL_SERVER_ERROR, false, ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
+    }
+}
